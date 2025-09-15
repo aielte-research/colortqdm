a75667a4933d9a56325f2775c1c15dd8e3c08cf0
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2025-09-15 15:38:20 +0200

    update env for testing pypi package

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["tqdm"],
+    install_requires=["tqdm==4.67.1"],
 )
